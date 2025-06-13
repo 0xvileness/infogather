22864bfcaecc8ea25b81c5ad7de2ea61c0844fc8
0xvileness <thatonedude9773@gmail.com>
2025-06-13 02:51:42 -0400

    Update info.py

--- info.py
+++ info.py
@@ -23,6 +23,6 @@ ip_addr = socket.gethostbyname(domain)
 print(f"IP address of {domain} is {ip_addr}","\n")
 
 req2 = requests.get ("https://ipinfo.io/"+ip_addr+"/json")
-data = json. loads (reg2.text)
+data = json. loads (req2.text)
 print ("Location of "+domain+" is "+data["loc"])
 print (data)
